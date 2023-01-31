6bdb5dc8ba0e5e91f94b19655fdaf8e1ddb4c07e
Jirayu Laungwilawan <jirayu.laungwilawan@gmail.com>
2023-01-31 05:03:40 +0000

    Add greenery dbt model

---
+++ greenery/models/greenery/users_with_purchases_model.sql
@@ -0,0 +1,20 @@
+with users_with_purchase AS (
+  SELECT
+    user_id,
+    CASE
+      WHEN COUNT(order_id) = 1 THEN 'one_order'
+      WHEN COUNT(order_id) = 2 THEN 'two_order'
+      WHEN COUNT(order_id) >= 3 THEN 'three_order'
+    END AS purchase
+  FROM
+    public.orders
+  GROUP BY
+    user_id
+)
+SELECT
+  purchase,
+  COUNT(purchase) AS user_count
+FROM
+  users_with_purchase
+GROUP BY
+  purchase
